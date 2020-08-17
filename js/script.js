f3088ebf5704324da237f10e343d2d99c94d6ff8
pgmann <pgmann@users.noreply.github.com>
2020-08-17 18:30:52 +0100

    Comment JS script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function changeSlide(slideshow, backward) {
+    // find the active slide within the slideshow, or use the first element
     var activeSlide = slideshow.querySelector(".active-slide") || slideshow.firstElementChild;
-    if (!activeSlide) return;
-    activeSlide.classList.remove("active-slide");
+    if (!activeSlide) return; // stop if there are no slides in the slideshow
+    activeSlide.classList.remove("active-slide"); // the current slide is no longer the active one
+    // determine next slide, it will be either the next sibling or the first slide if the end was reached
     var nextSlide;
     if (backward)
         nextSlide = activeSlide.previousElementSibling || slideshow.lastElementChild;
     else
         nextSlide = activeSlide.nextElementSibling || slideshow.firstElementChild;
-    nextSlide.classList.add("active-slide");
+    nextSlide.classList.add("active-slide"); // make the next slide active so it becomes visible
 }
